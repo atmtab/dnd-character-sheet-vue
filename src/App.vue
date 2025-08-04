ee3c6a33484b7f6deaa39ec47dec0eac83639c30
atmtab <75880836+atmtab@users.noreply.github.com>
2025-08-04 05:56:09 -0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <CharacterSheet />
+  </div>
+</template>
+
+<script setup>
+import CharacterSheet from './components/CharacterSheet.vue';
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+</style>
