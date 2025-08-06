c57727cc863663b058c30ecbe4e0fe1e7edd808d
atmtab <75880836+atmtab@users.noreply.github.com>
2025-08-05 21:28:28 -0700

    Create FeatureList.vue

---
+++ src/components/FeatureList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="feature-list">
+    <h2>Features up to Level {{ store.level }}</h2>
+    <ul v-if="features.length">
+      <li v-for="feature in features" :key="feature.name">
+        <strong>{{ feature.name }}</strong> (Level {{ feature.level }})
+      </li>
+    </ul>
+    <p v-else>No class selected.</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCharacterStore } from '../stores/characterStore';
+
+const store = useCharacterStore();
+const features = computed(() => store.classFeaturesForLevel);
+</script>
+
+<style scoped>
+.feature-list {
+  margin-top: 1rem;
+}
+.feature-list ul {
+  list-style: none;
+  padding-left: 0;
+}
+.feature-list li {
+  margin-bottom: 0.25rem;
+}
+</style>
